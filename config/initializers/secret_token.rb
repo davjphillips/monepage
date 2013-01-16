b163b66a9104bc0c20835105d9d5a1b504028118
David J. Phillips <d@sutterstudios.com>
2013-01-16 05:03:51 -0800

    updated to pg for heroku:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Doughify::Application.config.secret_token = 'adfa3d2eca1c81e660a885235cc545d50db3e6742bda45255da19c887e338b26bb8743f247e8da1509bc1fc548315a46c1e3c372d54cf17275414d45ad390e33'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Doughify::Application.config.session_store :cookie_store, key: '_doughify_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Doughify::Application.config.session_store :active_record_store
